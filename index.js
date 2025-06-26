69ee4fe901775fa35fef3bd9731f5267eadcff8c
Stephen <stephenmburumburu82@gmail.com>
2025-06-26 03:43:22 +0300

    phase-1-js-fetch-on-demand-with-forms

---
+++ index.js
@@ -0,0 +1,30 @@
+const init = () => {
+  const inputForm = document.querySelector("form");
+
+  inputForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    const input = document.querySelector("input#searchByID");
+    const movieId = input.value;
+
+    fetch(`http://localhost:3000/movies/${movieId}`)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Movie not found");
+        }
+        return response.json();
+      })
+      .then((data) => {
+        const title = document.querySelector("section#movieDetails h4");
+        const summary = document.querySelector("section#movieDetails p");
+
+        title.innerText = data.title;
+        summary.innerText = data.summary;
+      })
+      .catch((error) => {
+        alert(error.message);
+      });
+  });
+};
+
+document.addEventListener("DOMContentLoaded", init);
